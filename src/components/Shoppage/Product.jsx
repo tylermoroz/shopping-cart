510c17d3dec670353581c87ed04d7798a8c9d35e
tylermoroz <moroztyler@gmail.com>
2025-06-10 15:55:22 -0600

    Add tables for the weapon stats fetched from the api.

--- src/components/Shoppage/Product.jsx
+++ src/components/Shoppage/Product.jsx
@@ -3,8 +3,73 @@ const Product = ({ product }) => {
     <div className="product-card">
       <h3>{product.name}</h3>
       <p>{product.description}</p>
-      <p>{product.weight}</p>
+      <table border="1" style={{ color: "white", borderCollapse: "collapse" }}>
+        <thead>
+          <tr>
+            <th colSpan="2">Attack</th>
+          </tr>
+        </thead>
+        <tbody>
+          {product.attack.map((type) => (
+            <tr>
+              <td key={type.name}>{type.name}</td>
+              <td key={type.amount}>{type.amount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <table border="1" style={{ color: "white", borderCollapse: "collapse" }}>
+        <thead>
+          <tr>
+            <th colSpan="2">Guard</th>
+          </tr>
+        </thead>
+        <tbody>
+          {product.defence.map((type) => (
+            <tr>
+              <td key={type.name}>{type.name}</td>
+              <td key={type.amount}>{type.amount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <table border="1" style={{ color: "white", borderCollapse: "collapse" }}>
+        <thead>
+          <tr>
+            <th colSpan="2">Scaling</th>
+          </tr>
+        </thead>
+        <tbody>
+          {product.scalesWith.map((type) => (
+            <tr>
+              <td key={type.name}>{type.name}</td>
+              <td key={type.scaling}>{type.scaling}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <table border="1" style={{ color: "white", borderCollapse: "collapse" }}>
+        <thead>
+          <tr>
+            <th colSpan="2">Requires</th>
+          </tr>
+        </thead>
+        <tbody>
+          {product.requiredAttributes.map((type) => (
+            <tr>
+              <td key={type.name}>{type.name}</td>
+              <td key={type.amount}>{type.amount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
       <p>{product.category}</p>
+      <table border="1" style={{ color: "white", borderCollapse: "collapse" }}>
+        <tr>
+          <td>Weight</td>
+          <td>{product.weight}</td>
+        </tr>
+      </table>
     </div>
   );
 };
